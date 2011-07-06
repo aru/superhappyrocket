a5700a0a497276e8861cb07153eb11ec1aea5296
Katzider <eduardo.morales.fi@gmail.com>
2011-07-05 19:54:41 -0500

    SHR Xcode project up and running :)
    Just created the SHR base template for Mac OS X with OUR main.cpp running correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -17,98 +17,98 @@ void InitGL(int Width, int Height)	        // We call this right after our OpenG
 	// General suggested outline
 	// mcamera->setFrustum()
 	// mcamera->setFrame( camPosition, camDVector, camUVector, camRVector );
-
+	
 	// CreateScene();
-
+	
 	// Set up the camera
 	// Initial update of objects
 	// scene->update();
 	// Initial culling of scene.
-
+	
 	glViewport(0, 0, Width, Height);
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// This Will Clear The Background Color To Black
 	glClearDepth(1.0);						// Enables Clearing Of The Depth Buffer
 	glDepthFunc(GL_LESS);					// The Type Of Depth Test To Do
 	glEnable(GL_DEPTH_TEST);				// Enables Depth Testing
 	glShadeModel(GL_SMOOTH);				// Enables Smooth Color Shading
-
+	
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();						// Reset The Projection Matrix
-
+	
 	gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);	// Calculate The Aspect Ratio Of The Window
-
+	
 	glMatrixMode(GL_MODELVIEW);
 }
 
 /* The main drawing function. */
 void DrawGLScene()
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
-  glLoadIdentity();				// Reset The View
-
-  glTranslatef(-1.5f,0.0f,-6.0f);		// Move Left 1.5 Units And Into The Screen 6.0
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
+	glLoadIdentity();				// Reset The View
 	
-  // draw a triangle
-  glBegin(GL_POLYGON);				// start drawing a polygon
-  glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
-  glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
-  glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
-  glEnd();					// we're done with the polygon
-
-  glTranslatef(3.0f,0.0f,0.0f);		        // Move Right 3 Units
+	glTranslatef(-1.5f,0.0f,-6.0f);		// Move Left 1.5 Units And Into The Screen 6.0
 	
-  // draw a square (quadrilateral)
-  glBegin(GL_QUADS);				// start drawing a polygon (4 sided)
-  glVertex3f(-1.0f, 1.0f, 0.0f);		// Top Left
-  glVertex3f( 1.0f, 1.0f, 0.0f);		// Top Right
-  glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
-  glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
-  glEnd();					// done with the polygon
-
-  // swap buffers to display, since we're double buffered.
-  SDL_GL_SwapBuffers();
+	// draw a triangle
+	glBegin(GL_POLYGON);				// start drawing a polygon
+	glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
+	glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
+	glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
+	glEnd();					// we're done with the polygon
+	
+	glTranslatef(3.0f,0.0f,0.0f);		        // Move Right 3 Units
+	
+	// draw a square (quadrilateral)
+	glBegin(GL_QUADS);				// start drawing a polygon (4 sided)
+	glVertex3f(-1.0f, 1.0f, 0.0f);		// Top Left
+	glVertex3f( 1.0f, 1.0f, 0.0f);		// Top Right
+	glVertex3f( 1.0f,-1.0f, 0.0f);		// Bottom Right
+	glVertex3f(-1.0f,-1.0f, 0.0f);		// Bottom Left	
+	glEnd();					// done with the polygon
+	
+	// swap buffers to display, since we're double buffered.
+	SDL_GL_SwapBuffers();
 }
 
 int main(int argc, char **argv) 
 {  
-  int done;
-
-  /* Initialize SDL for video output */
-  if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
-    fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
-    exit(1);
-  }
-
-  /* Create a 640x480 OpenGL screen */
-  if ( SDL_SetVideoMode(640, 480, 0, SDL_OPENGL) == NULL ) {
-    fprintf(stderr, "Unable to create OpenGL screen: %s\n", SDL_GetError());
-    SDL_Quit();
-    exit(2);
-  }
-
-  /* Set the title bar in environments that support it */
-  SDL_WM_SetCaption("Jeff Molofee's GL Code Tutorial ... NeHe '99", NULL);
-
-  /* Loop, drawing and checking events */
-  InitGL(640, 480);
-  done = 0;
-  while ( ! done ) {
-    DrawGLScene();
-
-    /* This could go in a separate function */
-    { SDL_Event event;
-      while ( SDL_PollEvent(&event) ) {
-        if ( event.type == SDL_QUIT ) {
-          done = 1;
-        }
-        if ( event.type == SDL_KEYDOWN ) {
-          if ( event.key.keysym.sym == SDLK_ESCAPE ) {
-            done = 1;
-          }
-        }
-      }
-    }
-  }
-  SDL_Quit();
-  return 1;
+	int done;
+	
+	/* Initialize SDL for video output */
+	if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
+		fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	
+	/* Create a 640x480 OpenGL screen */
+	if ( SDL_SetVideoMode(640, 480, 0, SDL_OPENGL) == NULL ) {
+		fprintf(stderr, "Unable to create OpenGL screen: %s\n", SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+	
+	/* Set the title bar in environments that support it */
+	SDL_WM_SetCaption("Jeff Molofee's GL Code Tutorial ... NeHe '99", NULL);
+	
+	/* Loop, drawing and checking events */
+	InitGL(640, 480);
+	done = 0;
+	while ( ! done ) {
+		DrawGLScene();
+		
+		/* This could go in a separate function */
+		{ SDL_Event event;
+			while ( SDL_PollEvent(&event) ) {
+				if ( event.type == SDL_QUIT ) {
+					done = 1;
+				}
+				if ( event.type == SDL_KEYDOWN ) {
+					if ( event.key.keysym.sym == SDLK_ESCAPE ) {
+						done = 1;
+					}
+				}
+			}
+		}
+	}
+	SDL_Quit();
+	return 1;
 }
