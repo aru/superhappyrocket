b554d055e2efde7595afea3f08b53398f1eb8b00
popoca <el_chav0_del_8@hotmail.com>
2013-11-22 01:03:00 -0600

    Headers in all .h's for documentation

--- src/Level1.h
+++ src/Level1.h
@@ -1,3 +1,20 @@
+/**
+ * Level1.h
+ *
+ * This derived class comes from the base class BaseLevel, it is used
+ * to generate the first level used on the Super Happy Rocket project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details http://www.gnu.org/licenses/.
+ */
+
 #ifndef SHR_LEVEL1
 #define SHR_LEVEL1
 
