0ce95d4495f07c0adca8a3a607453a6fdb6c6621
aru <albinkun@gmail.com>
2016-06-06 15:16:25 -0500

    Separating linux from os x build step 1: sdl

--- CMakeModules/get-sdl2.cmake
+++ CMakeModules/get-sdl2.cmake
@@ -31,23 +31,52 @@ if (WIN32)
     #set(SDL2_LIBRARY ${SDL2_LIBRARY} ${DEPENDENCIES_ROOT}/lib/x86/SDL2main.lib)
     #endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
     set(SDL2_FOUND "YES")
-else() # Now, if using anything else, get the sources. (TODO Poor MacOSX guys have nothing yet. :)
-  # Build SDL2
+endif(WIN32)
+
+# Now, if using anything else, get the sources.
+if(APPLE)
+  # If we are using OS X
+  # Get SDL2 dev package
   ExternalProject_Add(
     sdl2
     PREFIX ${DEPENDENCIES_ROOT}
     DOWNLOAD_DIR ${DOWNLOAD_DIR}
-    URL https://libsdl.org/release/SDL2-2.0.4.tar.gz
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${DEPENDENCIES_ROOT}
+    URL http://libsdl.org/release/SDL2-2.0.4.dmg
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
     )
-  # experimental
-  set(SDL2_INCLUDE_DIR ${DEPENDENCIES_ROOT}/src/sdl2/include)
+  set(SDL2_INCLUDE_DIR ${DEPENDENCIES_ROOT}/src/sdl2.framework/headers)
+  #set(SDL2_INCLUDE_DIR ${DEPENDENCIES_ROOT}/include/SDL2)
+  #if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  #  link_directories(${DEPENDENCIES_ROOT}/lib/x64/)
+  #else(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(SDL2_LIBRARY_DIR ${DEPENDENCIES_ROOT}/src/sdl2/versions/current/)
+  set(SDL2_LIBRARY ${SDL2_LIBRARY} ${SDL2_LIBRARY_DIR}/SDL2)
+  #set(SDL2_LIBRARY ${SDL2_LIBRARY} ${DEPENDENCIES_ROOT}/src/sdl2/lib/x86/SDL2main.lib)
+  #set(SDL2_LIBRARY ${SDL2_LIBRARY} ${DEPENDENCIES_ROOT}/lib/x86/SDL2.lib)
+  #set(SDL2_LIBRARY ${SDL2_LIBRARY} ${DEPENDENCIES_ROOT}/lib/x86/SDL2main.lib)
+  #endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
   set(SDL2_FOUND "YES")
+endif(APPLE)
 
-  set(SDL2_LIBRARY_DIR ${DEPENDENCIES_ROOT}/lib)
-  set(SDL2_LIBRARY ${SDL2_LIBRARY} ${SDL2_LIBRARY_DIR}/libSDL2.so)
-  set(SDL2_LIBRARY ${SDL2_LIBRARY} ${SDL2_LIBRARY_DIR}/libSDL2main.a)
-endif()
+if(CMAKE_SYSTEM_NAME STREQUAL Linux)
+    # If we are using Linux
+    ExternalProject_Add(
+      sdl2
+      PREFIX ${DEPENDENCIES_ROOT}
+      DOWNLOAD_DIR ${DOWNLOAD_DIR}
+      URL https://libsdl.org/release/SDL2-2.0.4.tar.gz
+      CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${DEPENDENCIES_ROOT}
+      )
+    # experimental
+    set(SDL2_INCLUDE_DIR ${DEPENDENCIES_ROOT}/src/sdl2/include)
+    set(SDL2_FOUND "YES")
+
+    set(SDL2_LIBRARY_DIR ${DEPENDENCIES_ROOT}/lib)
+    set(SDL2_LIBRARY ${SDL2_LIBRARY} ${SDL2_LIBRARY_DIR}/libSDL2.so)
+    set(SDL2_LIBRARY ${SDL2_LIBRARY} ${SDL2_LIBRARY_DIR}/libSDL2main.a)
+endif(CMAKE_SYSTEM_NAME STREQUAL Linux)
 
 # find_path( SDL2_INCLUDE_DIR SDL.H
 #   PATH_SUFFIXES include/SDL2 include SDL2
